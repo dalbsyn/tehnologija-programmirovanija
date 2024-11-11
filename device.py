be3af37571618924aa0cc93873e1aa252ec5f258
fsfsnfmdkdo <aydana2005a@gmail.com>
2024-11-11 14:37:32 +0500

    message

--- device.py
+++ device.py
@@ -1,29 +1,51 @@
 class Device:
-    def __init__(self, name, battery_life):
-        self.__name = name
-        self.__battery_life = battery_life
+    def init(self, name, battery_life):
+        self._name = name
+        self._battery_life = battery_life
 
     def get_name(self):
-        return self.__name
+        return self._name
 
     def set_name(self, name):
-        self.__name = name
+        self._name = name
 
     def get_battery_life(self):
-        return self.__battery_life
+        return self._battery_life
 
     def set_battery_life(self, battery_life):
-        self.__battery_life = battery_life
+        self._battery_life = battery_life
 
-device = Device("laptop", 6)
+# 2. Наследование
+class Smartphone(Device):
+    def call(self):
+        print("Телефон звонит")
+    
+    def use(self):
+        print("Смартфон используется для звонков и приложений")
 
-print(device.get_name())          
-print(device.get_battery_life())   
+class Laptop(Device):
+    def compile_code(self):
+        print("Ноутбук компилирует код")
+    
+    def use(self):
+        print("Ноутбук используется для программирования и работы")
 
+class Tablet(Device):
+    def draw(self):
+        print("Планшет используется для рисования")
+    
+    def use(self):
+        print("Планшет используется для мультимедиа и рисования")
 
-device.set_name("Phone")
-device.set_battery_life(10)
+# 3. Полиморфизм (Overriding)
+# Создаем объекты классов
+smartphone = Smartphone("iPhone", 80)
+laptop = Laptop("MacBook", 90)
+tablet = Tablet("iPad", 70)
 
+# Список с объектами
+devices = [smartphone, laptop, tablet]
 
-print(device.get_name())          
-print(device.get_battery_life())   
+# Вызов метода use() для каждого объекта
+for device in devices:
+    device.use()
