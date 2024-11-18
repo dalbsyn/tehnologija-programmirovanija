85b4590a53ac53a69179cbb8e50dc85ccc552dc7
R1maRoma <roma.martynyuk.06@mail.ru>
2024-11-18 16:40:20 +0500

    asfd

---
+++ Kr1.1.py
@@ -0,0 +1,37 @@
+#Вариант 1
+class Vehicle:
+   def__init__(self, speed, color):
+   self._speed=speed
+   self._color=color
+
+   def get_speed():
+   return self._speed
+
+   def set_speed():
+   if speed<0:
+      print("скорость не может быть отрицательной")
+   self._speed=speed
+
+   def get_color:
+   return self._color
+
+   def set_color:
+   self._color=color
+
+class Car(Vehicle):
+   def drive():
+       print("Машина едет")
+   def move(self):
+       print("Машина двигается")
+       
+class Bike(Vehicle):
+   def pedal():
+       print("Велосипед крутит педали")
+   def move():
+       print("Велосипед едет")
+       
+class Boat(Vehicle):
+   def sail():
+       print("Лодка плывёт")
+   def move():   
+       print("Лодка плывёт")
