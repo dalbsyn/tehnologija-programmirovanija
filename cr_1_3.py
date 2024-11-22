8c693f2b2403ee958931e087a51674b8cc3979ab
VadimRustam <vg1226205@gmail.com>
2024-11-22 18:38:51 +0500

    1_ct

---
+++ cr_1_3.py
@@ -0,0 +1,66 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.__account_number = account_number
+        self.__balance = balance
+    
+    def set_account_number(self, account_number):
+        self.__account_number = account_number
+
+    def set_balance(self, balance):
+        self.__balance = balance
+
+    def get_account_number(self):
+        return account_number
+
+    def get_balance(self):
+        return balance
+
+    def deposit(self, balance):
+        def set_balance(self):
+            return balance
+
+    def withdraw(self, balance):
+        def get_balance(self):
+            if balance > 0:
+                return get_balance()
+
+class Vihicle:
+    def __init__(self, make, model, year):
+        self.make = male
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+        return self.make
+        return self.model
+        return self,year
+
+class Car(Vihicle):
+    def numbers_of_doors(self):
+        pass
+
+class Truch(Vihicle):
+    def payload_capacity(self):
+        pass
+
+class Animal:
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        print("Woof")
+
+
+class Cat(Animal):
+    def make_sound(self):
+        print("meom")
+
+
+class Bird(Animal):
+    def make_sound(self):
+        print("Chirp")
+        
+
+
+
