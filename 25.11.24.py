69a2f1def4541a1c9d579f60032091411819dc8f
Ilyasiksensei <128810270+Ilyasiksensei@users.noreply.github.com>
2024-11-25 21:44:26 +0500

    ғ

---
+++ 25.11.24.py
@@ -0,0 +1,13 @@
+class Players:
+	def_init__[fieldPlayer, Goalkeeper]:
+		self. FieldPlayer = FieldPlayer
+	def show_FieldPlayer:
+		print("Какой фуболист?{self.FieldPlayer}")
+Players_age18 = Players_age 18 ("Молодой")
+Players_age18: show_FieldPlayer
+
+Players_age30 = Players_age 25 ("В форме")
+Players_age45: show_FieldPlayer()
+
+Players_age45 = Players_age 18 ("Еше пробежит")
+Players_age45: show_FieldPlayer()
