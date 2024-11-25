5555ca7eb60d11698c4242e4074a88e65fa60648
alfarabi991 <makashev_05@mail.ru>
2024-11-25 16:16:42 +0500

    initial commit

---
+++ 2kr_2var.py
@@ -0,0 +1,17 @@
+#2 вариант
+class City:
+   def__init__(road,building,park)
+     self.road = road
+     self.building = building
+     self.park = park
+def road (self):
+     print(f"{self.road}Длинные дороги")
+def park (self):
+     print(f"{self.park}Большой парк")
+def building (self):
+     print(f"{self.building}Высокие здания")
+
+
+
+class Infrastructure:
+   def
