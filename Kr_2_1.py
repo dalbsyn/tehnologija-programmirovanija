abf0def37cd9507c590a08590a81994e1b7bb714
R1maRoma <roma.martynyuk.06@mail.ru>
2024-11-25 16:21:07 +0500

    a

---
+++ Kr_2_1.py
@@ -0,0 +1,18 @@
+class University:
+    def __init__(self, faculty):
+        self.__faculty = faculty
+    def get_faculty():
+        return self.__faculty
+    def set_faculty():
+        self.__faculty = faculty
+class Faculty(University):
+    def __init__(self, groups):
+        self.__groups = groups
+    def get_groups():
+        return groups
+    def set_groups():
+        self.__groups = groups
+    def add():
+        print("добавлен новый факультет")
+    def delete():
+        print("факультет удалён") 
