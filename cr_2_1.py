75d476107ee203b1d35d14bb38d79ab4e8f3bdc1
VadimRustam <vg1226205@gmail.com>
2024-11-27 16:01:14 +0500

    kjdfs

--- cr_2_1.py
+++ cr_2_1.py
@@ -1,5 +1,23 @@
 #1 агрегация
 class A:
+    def __init__(self, a):
+        self.a = a
 
 class B:
+    def __init__(self, b):
+        self.b = b
+
+a = A("Y")
+b = B(a)
+
 #2 компоизиция
+
+class C:
+    def __init__(self):
+        pass
+
+class D:
+    def __init__(self):
+        self.d = C()
+
+d = D("jfd")
