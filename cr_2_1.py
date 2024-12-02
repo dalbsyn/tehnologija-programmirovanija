976c87633514a4679a64b4e57d658568130fdfba
VadimRustam <vg1226205@gmail.com>
2024-12-02 21:26:08 +0500

    .

--- cr_2_1.py
+++ cr_2_1.py
@@ -1,23 +1,59 @@
-#1 агрегация
-class A:
-    def __init__(self, a):
-        self.a = a
+# 1 игрок и команда
 
-class B:
-    def __init__(self, b):
-        self.b = b
+class Player:
+    def __init__(self, age):
+        self.age = age
 
-a = A("Y")
-b = B(a)
 
-#2 компоизиция
+class Team:
+    def __init__(self, age):
+        self.age = age
+        self.spisok = []
+    
+    def add_in_team(self, age):
+        return self.spisok.append(age)
 
-class C:
-    def __init__(self):
-        pass
+    def del_in_team(self, element):
+        return self.spisok.remove(element)
 
-class D:
-    def __init__(self):
-        self.d = C()
+    def cred_chislo(self):
+        summ = 0
+        for i in self.sposok:
+            summ += age.i
+        print(summ)
 
-d = D("jfd")
+# 2 барбара
+
+class Player:
+    def __init__(self, age):
+        self.age = age
+
+class FieldPlayer:
+    def __init__(self, age, weight):
+        super().__init__(age)
+        self.weight = weight
+
+class Goalkeep:
+    def __init__(self, age, weight):
+        super().__init__(age)
+        self.weight = weight
+
+class Team:
+    def __init__(self, age):
+        super().__init__(age)
+        self.spisok = []
+
+    def add_player(self, element):
+        self.spisok.append(element)
+
+    def cred_chislo(self):
+        summ = 0
+        for i in self.spisok:
+            summ += age.i
+        print(summ)
+
+player1 = Player()
+player1.add_player(10)
+
+
+
